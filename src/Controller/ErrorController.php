66c98911ad0316e4a35c3388ce4f3074bbac9d07
Thierry Dothee <thierrydothee@protonmail.com>
2025-06-18 16:53:46 +0200

    implement error pages

--- src/Controller/ErrorController.php
+++ src/Controller/ErrorController.php
@@ -12,7 +12,8 @@ class ErrorController extends AbstractController
     #[Route('/error/{e}', name: 'app_error')]
     public function index(Exception $e): Response
     {
-        return $this->render('bundles/TwigBundle/Exception/error.html.twig',
-            ['status_code'=>'error_404','status_text'=>'Impossible d\'envoyer le mail']);
+
+        return $this->render('bundles/TwigBundle/Exception/error'.$e->getCode(). '.html.twig',
+            ['status_code'=>$e->getCode(),'status_text'=>$e->getMessage()]);
     }
 }
